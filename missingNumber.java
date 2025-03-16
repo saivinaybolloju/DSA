c4ae56e6f06f9380cdd3fed661b226afb603d0df
saivinaybolloju <saivinay.bolloju@gmail.com>
2025-03-16 12:50:11 +0530

    missingNumberQuestion

---
+++ missingNumber.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int n=nums.length;
+        int sum=0;
+        for(int x :nums){
+            sum=sum+x;
+        }
+        return ((n*(n+1)/2)-sum);
+    }
+}
