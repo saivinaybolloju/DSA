85005848313fa72e29f06b640bd2475bcdba3d36
saivinaybolloju <saivinay.bolloju@gmail.com>
2025-07-26 23:40:08 +0530

    Frst Missing Positive -Optimised- LC

---
+++ Arrays/FrstMissingPositive.java
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/first-missing-positive/
+
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] <= 0 || nums[i] > nums.length) {
+                nums[i] = nums.length + 1;
+            }
+        }
+       for (int i = 0; i < nums.length; i++) {
+        if (Math.abs(nums[i]) > nums.length) continue; 
+        if(nums[Math.abs(nums[i])-1]>0)
+            nums[Math.abs(nums[i])-1]=-1*nums[Math.abs(nums[i])-1];
+       }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                return i+1;
+            }
+        }
+        return nums.length+1;       
+    }
+}
