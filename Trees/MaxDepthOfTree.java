26385678f91e4c0694726e39f3711303effa0d79
saivinaybolloju <saivinay.bolloju@gmail.com>
2025-08-02 22:26:30 +0530

    Max Depth Of Tree - LC

---
+++ Trees/MaxDepthOfTree.java
@@ -0,0 +1,26 @@
+//https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if(root==null)
+            return 0;
+        int lefth=maxDepth(root.left);
+        int righth=maxDepth(root.right);
+        return 1+Math.max(lefth,righth);
+    }
+}
