1118db192b40659d66f91dffdcdf0ad8916232c7
saivinaybolloju <saivinay.bolloju@gmail.com>
2025-11-08 00:12:18 +0530

    Delete Middle of LL - Extra Pointer + Floyd's Cycle intution

---
+++ LinkedLists/DeleteMiddleOfLL.java
@@ -0,0 +1,19 @@
+//https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+
+class Solution {
+    public ListNode deleteMiddle(ListNode head) {
+        if(head==null)  return null;
+        if(head.next==null) return null;
+        
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode prev=null;
+        while(fast!=null&&fast.next!=null){
+            prev=slow;
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        prev.next=prev.next.next;
+        return head;
+    }
+}
